5e590c1b3a4d301bc7c7099d14dbf249105cbd95
Arix <arixgg@gmail.com>
2022-08-02 23:53:06 +0800

    Create key.ts

---
+++ waidh-fe/src/utils/key.ts
@@ -0,0 +1,29 @@
+export enum SceneKey {
+	LOADER = 'LOADER',
+	LOGIN = 'LOGIN',
+}
+
+export enum MapKey {
+	FOREST = 'FOREST',
+	// PARK = 'PARK',
+	// STREET = 'STREET',
+	// THIEF_TOWN = 'THIEF_TOWN',
+	// NIGHT_TOWN = 'NIGHT_TOWN',
+}
+
+export enum BackgroundKey {
+  LOGIN_BACKGROUND = 'LOGIN_BACKGROUND',
+}
+
+export enum SoundKey {
+  LOGIN = "LOGIN_AUDIO"
+}
+
+export enum SettingKey {
+  SOUND_VOLUMNE = 'SOUND-VOLUME'
+}
+
+export enum SpriteKey {
+  PINK_BEAN = 'PINK_BEAN_SPRITE',
+  PORTAL = 'PORTAL_SPRITE',
+}
