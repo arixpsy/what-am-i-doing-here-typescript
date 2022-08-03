40aacabf17ed8385b82d59c942bf58c9df9fc72c
Arix <arixgg@gmail.com>
2022-08-03 23:44:16 +0800

    update sound

--- waidh-fe/src/utils/sound.ts
+++ waidh-fe/src/utils/sound.ts
@@ -2,6 +2,9 @@ import { SoundKey } from './key'
 import titleAudio from './../assets/sounds/title.mp3'
 import cavaBienAudio from './../assets/sounds/CavaBien.mp3'
 
+import jumpAudio from './../assets/sounds/effects/Jump.mp3'
+import portalAudio from './../assets/sounds/effects/Portal.mp3'
+
 type SoundDataShape = {
 	key: SoundKey
 	audio: string
@@ -15,6 +18,16 @@ const SoundData: Record<SoundKey, SoundDataShape> = {
 	[SoundKey.CAVA_BIEN]: {
 		key: SoundKey.CAVA_BIEN,
 		audio: cavaBienAudio,
+	},
+
+	// EFFECTS
+	[SoundKey.JUMP]: {
+		key: SoundKey.JUMP,
+		audio: jumpAudio,
+	},
+	[SoundKey.PORTAL]: {
+		key: SoundKey.PORTAL,
+		audio: portalAudio,
 	},
 }
 
