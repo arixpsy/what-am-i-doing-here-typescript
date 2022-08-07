33d5a6df0cdf6b8fe7fd6f0c653aef2a58b215e3
Arix <arixgg@gmail.com>
2022-08-08 05:17:06 +0800

    complete login for socket BE

--- waidh-be/src/socket.ts
+++ waidh-be/src/socket.ts
@@ -1,10 +1,41 @@
-import http from 'http'
 import { Server } from 'socket.io'
+import { MapKey } from './utils/constants'
+import { map } from 'zod'
+import { ILogin, IPlayerInfo, SocketEvent, ZLoginSchema } from './utils/socket'
 
 const socket = (io: Server) => {
-  io.on('connection', (socket) => {
-    console.log('a user connected');
-  });
+	io.use((socket, next) => {
+		const login = socket.handshake.auth
+		try {
+			ZLoginSchema.parse(login)
+		} catch (err) {
+			return next(new Error(JSON.stringify(err)))
+		}
+		next()
+	})
+
+	io.on(SocketEvent.CONNECTED, (socket) => {
+		const playerInfo: IPlayerInfo = {
+			...(socket.handshake.auth as ILogin),
+			socketId: socket.id,
+			channel: 1,
+			map: MapKey.FOREST,
+			portal: 0,
+		}
+		const roomId = `${playerInfo.map}-${playerInfo.channel}`
+		socket.join(roomId)
+
+		console.log('A user connected: ' + playerInfo.uid)
+		console.log('A user join room: ' + roomId)
+
+		io.to(socket.id).emit(SocketEvent.LOGIN_SUCCESS, playerInfo)
+
+		socket.on(SocketEvent.JOIN_MAP, async () => {})
+
+		socket.on(SocketEvent.DISCONNECT, async function () {
+			console.log('A user disconnected: ' + playerInfo.uid)
+		})
+	})
 }
 
 export default socket
---
+++ waidh-be/src/utils/socket.ts
@@ -0,0 +1,29 @@
+import z from 'zod'
+import { MapKey, SpriteKey } from './constants'
+
+export enum SocketEvent {
+  // TO SERVER
+  CONNECTED = 'connection',
+  DISCONNECT = 'disconnect',
+	LOGIN_SUCCESS = 'login_success',
+  JOIN_MAP = 'join_map',
+
+  // TO CLIENT
+}
+
+export const ZLoginSchema = z.object({
+	uid: z.string(),
+	displayName: z.string(),
+	spriteType: z.nativeEnum(SpriteKey),
+})
+
+export type ILogin = z.infer<typeof ZLoginSchema>
+
+export const ZPlayerInfoSchema = ZLoginSchema.extend({
+	socketId: z.string(),
+	channel: z.number(),
+	map: z.nativeEnum(MapKey),
+	portal: z.number(),
+})
+
+export type IPlayerInfo = z.infer<typeof ZPlayerInfoSchema>
