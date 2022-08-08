c3c629f75c58b46e41fd71a2635550377f43e4ec
Arix <arixgg@gmail.com>
2022-08-08 19:19:51 +0800

    add new sound

--- waidh-fe/src/utils/sound.ts
+++ waidh-fe/src/utils/sound.ts
@@ -1,6 +1,7 @@
 import { SoundKey } from './key'
 import titleAudio from './../assets/sounds/title.mp3'
 import cavaBienAudio from './../assets/sounds/CavaBien.mp3'
+import myPrinceMyKingdomAudio from './../assets/sounds/MyPrinceMyKingdom.mp3'
 
 import jumpAudio from './../assets/sounds/effects/Jump.mp3'
 import portalAudio from './../assets/sounds/effects/Portal.mp3'
@@ -15,10 +16,15 @@ const SoundData: Record<SoundKey, SoundDataShape> = {
 		key: SoundKey.LOGIN,
 		audio: titleAudio,
 	},
+	[SoundKey.MY_PRINCE_MY_KINGDOM]: {
+		key: SoundKey.MY_PRINCE_MY_KINGDOM,
+		audio: myPrinceMyKingdomAudio,
+	},
 	[SoundKey.CAVA_BIEN]: {
 		key: SoundKey.CAVA_BIEN,
 		audio: cavaBienAudio,
 	},
+
 
 	// EFFECTS
 	[SoundKey.JUMP]: {
