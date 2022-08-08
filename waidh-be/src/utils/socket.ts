a8f45ad5cde2265da3d7a415cb901f5ac2280513
Arix <arixgg@gmail.com>
2022-08-09 04:16:01 +0800

    add new socket events

--- waidh-be/src/utils/socket.ts
+++ waidh-be/src/utils/socket.ts
@@ -7,8 +7,11 @@ export enum SocketEvent {
   DISCONNECT = 'disconnect',
 	LOGIN_SUCCESS = 'login_success',
   JOIN_MAP = 'join_map',
+	REQUEST_ALL_PLAYERS = 'request_all_players',
 
   // TO CLIENT
+	PLAYER_CONNECT = 'player_connect',
+	PLAYER_DISCONNECT = 'player_disconnect',
 }
 
 export const ZLoginSchema = z.object({
--- waidh-fe/src/utils/socket.ts
+++ waidh-fe/src/utils/socket.ts
@@ -1,9 +1,12 @@
 export enum SocketEvent {
-	// TO SERVER
+  // TO SERVER
+  CONNECTED = 'connection',
+  DISCONNECT = 'disconnect',
 	LOGIN_SUCCESS = 'login_success',
-	JOIN_MAP = 'join_map',
+  JOIN_MAP = 'join_map',
+	REQUEST_ALL_PLAYERS = 'request_all_players',
 
-	// TO CLIENT
-	CONNECTED = 'connection',
-	DISCONNECT = 'disconnect',
+  // TO CLIENT
+	PLAYER_CONNECT = 'player_connect',
+	PLAYER_DISCONNECT = 'player_disconnect',
 }
