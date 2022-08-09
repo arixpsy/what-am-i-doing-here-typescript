961b0548db4c8192fc061e2b0dd6f96bf785a26d
Arix <arixgg@gmail.com>
2022-08-09 22:07:43 +0800

    update socketevent enum

--- waidh-be/src/utils/socket.ts
+++ waidh-be/src/utils/socket.ts
@@ -2,16 +2,20 @@ import z from 'zod'
 import { MapKey, SpriteKey } from './constants'
 
 export enum SocketEvent {
-  // TO SERVER
-  CONNECTED = 'connection',
-  DISCONNECT = 'disconnect',
+	// TO SERVER
+	CONNECTED = 'connection',
+	DISCONNECT = 'disconnect',
 	LOGIN_SUCCESS = 'login_success',
-  JOIN_MAP = 'join_map',
+	JOIN_MAP = 'join_map',
 	REQUEST_ALL_PLAYERS = 'request_all_players',
+	CLIENT_MOVEMENT = 'client_movement',
+	CLIENT_MOVEMENT_STOP = 'client_movement_stop',
 
-  // TO CLIENT
+	// TO CLIENT
 	PLAYER_CONNECT = 'player_connect',
 	PLAYER_DISCONNECT = 'player_disconnect',
+	PLAYER_MOVE = 'player_move',
+	PLAYER_STOP = 'player_stop',
 }
 
 export const ZLoginSchema = z.object({
