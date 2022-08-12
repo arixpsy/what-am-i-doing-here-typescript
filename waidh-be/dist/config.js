ddda5b665e1172f74a11c447e059d56bd4ccafda
Phua Si Yu Arix <42609059+arixpsy@users.noreply.github.com>
2022-08-13 02:22:18 +0800

    Delete waidh-be/dist directory

--- waidh-be/dist/config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CORS_ORIGIN = void 0;
-const constants_1 = require("./utils/constants");
-exports.CORS_ORIGIN = {
-    [constants_1.Environment.DEV]: ["http://localhost:8080"],
-    [constants_1.Environment.PROD]: [],
-};
